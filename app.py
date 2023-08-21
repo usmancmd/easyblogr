9816fc886a1089d3aaace6106a16caaed05f3401
Usman Adepoju <adepojuusman465@gmail.com>
2023-08-21 11:13:17 +0100

    Add navbars and pages

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import SignupForm, LoginForm, BlogPostForm, SearchForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
 
 
 @app.route('/')
@@ -16,6 +20,31 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+@app.route('/signup')
+def signup():
+	signup = SignupForm()
+	return render_template('signup.html', signup=signup)
+
+@app.route('/login')
+def login():
+	login = LoginForm()
+	return render_template('login.html', login=login)
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route('/nav')
+def nav():
+	# search = SearchForm()
+	return render_template('navbar1.html')
+
+@app.route('/blogs')
+def blogs():
+	search = SearchForm()
+	return render_template('blogs.html',  search=search)
+
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -7,19 +7,47 @@ from flask_ckeditor import CKEditorField
 from flask_wtf.file import FileField
 
 class SignupForm(FlaskForm):
-	firstname = StringField("firstname", validators=[DataRequired()])
-	lastname = StringField("lastname", validators=[DataRequired()])
-	email = StringField("email", validators=[DataRequired()])
+	firstname = StringField("firstname", render_kw={'placeholder': 'firstname'}, validators=[DataRequired()])
+	lastname = StringField("lastname", render_kw={'placeholder': 'lastname'}, validators=[DataRequired()])
+	username = StringField("username", render_kw={'placeholder': 'username'}, validators=[DataRequired()])
+	email = StringField("email", render_kw={'placeholder': 'email'}, validators=[DataRequired()])
 	about_author = TextAreaField("About Author")
-	password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords Must Match!')])
-	confirm_password = PasswordField('confirm password', validators=[DataRequired()])
+	password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), EqualTo('confirm_password', message='Passwords Must Match!')])
+	confirm_password = PasswordField('confirm password', render_kw={'placeholder': 'confirm password'}, validators=[DataRequired()])
 	profile_pic = FileField("Profile Pic")
-	submit = SubmitField("submit")
+	submit = SubmitField("signup")
 
 class LoginForm(FlaskForm):
-	email = StringField("enter your email", validators=[DataRequired()])
-	password = PasswordField('enter your password', validators=[DataRequired()])
-	submit = SubmitField("submit")
+	email = StringField("email", render_kw={'placeholder': 'email'}, validators=[DataRequired()])
+	password = PasswordField('password', render_kw={'placeholder': 'password'}, validators=[DataRequired()])
+	submit = SubmitField("login")
 
+class PostForm(FlaskForm):
+	username = StringField("username", render_kw={'placeholder': 'username'}, validators=[DataRequired()])
+	title = StringField("Title", render_kw={'placeholder': 'title'}, validators=[DataRequired()])
+	content = CKEditorField('Content', render_kw={'placeholder': 'start typing here'}, validators=[DataRequired()])
+	tag = StringField("Slug", render_kw={'placeholder': 'slug'}, validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
+class SearchForm(FlaskForm):
+	searchfield = StringField("Search", render_kw={'placeholder': 'search...'}, validators=[DataRequired()])
+	submit = SubmitField("Search")
 
+class BlogPostForm(FlaskForm):
+	username = StringField("username", render_kw={'placeholder': 'username'}, validators=[DataRequired()])
+	title = StringField("Title", render_kw={'placeholder': 'title'}, validators=[DataRequired()])
+
+	def validate_image(form, field):
+		if field.data:
+			file_size = len(field.data.read())
+			max_size = 10 * 1024 * 1024  # 10MB
+			if file_size > max_size:
+				raise ValidationError('File size must not exceed 10MB.')
+    
+		image = FileField('Image', validators=[
+		FileRequired(),
+		FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')
+		])
+	content = TextAreaField('Content', render_kw={'placeholder': 'start typing here...'}, validators=[DataRequired()])
+    
+	submit = SubmitField('Submit')
